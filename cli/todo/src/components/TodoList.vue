4e8afd928589e7b199b53008697e9aafa4226b32
Tomasz Sikora <sikora507@gmail.com>
2019-03-24 15:53:39 +0100

    todo2

--- cli/todo/src/components/TodoList.vue
+++ cli/todo/src/components/TodoList.vue
@@ -1,9 +1,20 @@
 <template>
-  <div>
-    <h1>To do list</h1>
-    <ul>
-      <li v-for="(todo, index) in thingsToDo" :key="index">{{todo}}</li>
-    </ul>
+  <div class="container">
+    <div class="holder">
+      <form @submit.prevent="addTodo">
+        <input
+          type="text"
+          placeholder="Enter new TODO item..."
+          v-model="newTodo"
+          v-validate="'min:5'"
+          name="newTodo"
+        >
+        <p class="alert" v-if="errors.has('newTodo')">{{errors.first('newTodo')}}</p>
+      </form>
+      <ul>
+        <li v-for="(todo, index) in thingsToDo" :key="index">{{todo}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -17,5 +28,49 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.holder {
+  background: #fff;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+}
+
+p {
+  text-align: center;
+  padding: 30px 0;
+  color: gray;
+}
+
+.container {
+  box-shadow: 0px 0px 40px lightgray;
+}
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+.alert {
+  background: #fdf2ce;
+  font-weight: bold;
+  display: inline-block;
+  padding: 5px;
+  margin-top: -20px;
+}
 </style>
+
