f43ecac0dc0f4105a0b846bd024cbb0cb2f8a6ab
Tomasz Sikora <sikora507@gmail.com>
2019-03-24 16:26:37 +0100

    todo3

--- cli/todo/src/components/TodoList.vue
+++ cli/todo/src/components/TodoList.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="container">
     <div class="holder">
+      <form @submit.prevent="addTodo">
+        <input type="text" placeholder="Enter new TODO item..." v-model="newTodo" name="newTodo">
+      </form>
       <ul>
-        <li v-for="(todo, index) in thingsToDo" :key="index">{{todo}}</li>
+        <li v-for="(todo, index) in thingsToDo" :key="index">
+            {{todo}}
+            <a class="remove-button" @click="function(){removeTodo(todo);}"></a>
+        </li>
       </ul>
     </div>
   </div>
@@ -12,8 +18,18 @@
 export default {
   data() {
     return {
+      newTodo: "",
       thingsToDo: ["Learn VUE", "Learn React", "Refresh JS knowledge"]
     };
+  },
+  methods: {
+    addTodo() {
+      this.thingsToDo.push(this.newTodo);
+      this.newTodo = "";
+    },
+    removeTodo(todo){
+        this.thingsToDo = this.thingsToDo.filter(x => x!=todo);
+    }
   }
 };
 </script>
@@ -36,16 +52,44 @@ ul li {
   border-left: 5px solid #3eb3f6;
   margin-bottom: 2px;
   color: #3e5252;
+  position: relative;
+  padding-right: 40px;
 }
-
-p {
-  text-align: center;
-  padding: 30px 0;
-  color: gray;
+ul li:hover a {
+    display: inherit;
 }
 
 .container {
   box-shadow: 0px 0px 40px lightgray;
 }
+input {
+  width: calc(100% - 40px);
+  border: 0;
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #323333;
+  color: #687f7f;
+}
+.remove-button{
+    display: none;
+    position:absolute;
+    right: 5px;
+    top: 15px;
+    width: 25px;
+    height: 25px;
+    text-align: center;
+    border: 2px solid #3eb3f6;
+    border-radius: 25px;
+    background-color: white;
+    cursor: pointer;
+    transition-duration: 0.4s;
+    content: "aa";
+}
+.remove-button:hover {
+  background-color:  #3eb3f6;
+  color: white;
+}
+.remove-button:after {
+  content: "\00d7";
+}
 </style>
-
