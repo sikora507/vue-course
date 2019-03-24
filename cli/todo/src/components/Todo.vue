9c35163f0418910710819870a7c1a38c5b6a6278
Tomasz Sikora <sikora507@gmail.com>
2019-03-24 17:50:30 +0100

    refactoring

---
+++ cli/todo/src/components/Todo.vue
@@ -0,0 +1,50 @@
+<template>
+    <li>
+        {{data.title}}
+        <a class="remove-button" @click="$emit('removeTodo', data)"></a>
+    </li>
+</template>
+<script>
+export default {
+    props: ['data']
+}
+</script>
+<style scoped>
+ul li {
+  padding: 20px;
+  font-size: 1.3em;
+  background-color: #e0edf4;
+  border-left: 5px solid #3eb3f6;
+  margin-bottom: 2px;
+  color: #3e5252;
+  position: relative;
+  padding-right: 40px;
+}
+ul li:hover a {
+  display: inherit;
+}
+.remove-button {
+  display: none;
+  position: absolute;
+  right: 5px;
+  top: 15px;
+  width: 25px;
+  height: 25px;
+  text-align: center;
+  border: 2px solid #3eb3f6;
+  border-radius: 25px;
+  background-color: white;
+  cursor: pointer;
+  transition-duration: 0.4s;
+  content: "aa";
+}
+.remove-button:hover {
+  background-color: #3eb3f6;
+  color: white;
+}
+.remove-button:after {
+  content: "\00d7";
+}
+</style>
+
+
--- cli/todo/src/components/TodoList.vue
+++ cli/todo/src/components/TodoList.vue
@@ -5,30 +5,40 @@
         <input type="text" placeholder="Enter new TODO item..." v-model="newTodo" name="newTodo">
       </form>
       <ul>
-        <li v-for="(todo, index) in thingsToDo" :key="index">
-            {{todo}}
-            <a class="remove-button" @click="() => removeTodo(todo)"></a>
-        </li>
+        <Todo
+          v-for="(todo, index) in todos"
+          :key="index"
+          :data="todo"
+          v-on:removeTodo="removeTodo"
+        />
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import Todo from "./Todo.vue";
 export default {
+  components: {
+    Todo
+  },
   data() {
     return {
       newTodo: "",
-      thingsToDo: ["Learn VUE", "Learn React", "Refresh JS knowledge"]
+      todos: [
+        { title: "Refresh JS knowledge", completed: true },
+        { title: "Learn VUE", completed: false },
+        { title: "Learn React", completed: false }
+      ]
     };
   },
   methods: {
     addTodo() {
-      this.thingsToDo.push(this.newTodo);
+      this.todos.push({ completed: false, title: this.newTodo });
       this.newTodo = "";
     },
-    removeTodo(todo){
-        this.thingsToDo = this.thingsToDo.filter(x => x!=todo);
+    removeTodo(todo) {
+      this.todos = this.todos.filter(x => x != todo);
     }
   }
 };
@@ -45,20 +55,6 @@ ul {
   list-style-type: none;
 }
 
-ul li {
-  padding: 20px;
-  font-size: 1.3em;
-  background-color: #e0edf4;
-  border-left: 5px solid #3eb3f6;
-  margin-bottom: 2px;
-  color: #3e5252;
-  position: relative;
-  padding-right: 40px;
-}
-ul li:hover a {
-    display: inherit;
-}
-
 .container {
   box-shadow: 0px 0px 40px lightgray;
   width: 50%;
@@ -70,27 +66,5 @@ input {
   font-size: 1.3em;
   background-color: #323333;
   color: #687f7f;
-}
-.remove-button{
-    display: none;
-    position:absolute;
-    right: 5px;
-    top: 15px;
-    width: 25px;
-    height: 25px;
-    text-align: center;
-    border: 2px solid #3eb3f6;
-    border-radius: 25px;
-    background-color: white;
-    cursor: pointer;
-    transition-duration: 0.4s;
-    content: "aa";
-}
-.remove-button:hover {
-  background-color:  #3eb3f6;
-  color: white;
-}
-.remove-button:after {
-  content: "\00d7";
 }
 </style>
