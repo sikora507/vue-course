5550e2f39e8ecd10a7b081ba71fcc5eb825ccfdb
Tomasz Sikora <sikora507@gmail.com>
2019-03-24 15:56:23 +0100

    styling

--- cli/todo/src/components/TodoList.vue
+++ cli/todo/src/components/TodoList.vue
@@ -1,16 +1,6 @@
 <template>
   <div class="container">
     <div class="holder">
-      <form @submit.prevent="addTodo">
-        <input
-          type="text"
-          placeholder="Enter new TODO item..."
-          v-model="newTodo"
-          v-validate="'min:5'"
-          name="newTodo"
-        >
-        <p class="alert" v-if="errors.has('newTodo')">{{errors.first('newTodo')}}</p>
-      </form>
       <ul>
         <li v-for="(todo, index) in thingsToDo" :key="index">{{todo}}</li>
       </ul>
@@ -56,21 +46,6 @@ p {
 
 .container {
   box-shadow: 0px 0px 40px lightgray;
-}
-input {
-  width: calc(100% - 40px);
-  border: 0;
-  padding: 20px;
-  font-size: 1.3em;
-  background-color: #323333;
-  color: #687f7f;
-}
-.alert {
-  background: #fdf2ce;
-  font-weight: bold;
-  display: inline-block;
-  padding: 5px;
-  margin-top: -20px;
 }
 </style>
 
