259ab6ff47b1fc9e09e5acbc93f6d6e07d88dfb1
Tomasz Sikora <sikora507@gmail.com>
2019-03-25 21:59:38 +0100

    custom validation text

--- cli/todo/src/main.js
+++ cli/todo/src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
 
 Vue.use(VeeValidate);
+const dictionary = {
+  en: {
+    messages:{
+      min: (fieldName, params) => `${fieldName} must be at least ${params[0]} characters long.`
+    },
+    attributes:{
+      item: 'TODO item'
+    }
+  }
+};
+Validator.localize(dictionary);
+
 Vue.config.productionTip = false
 
 new Vue({
